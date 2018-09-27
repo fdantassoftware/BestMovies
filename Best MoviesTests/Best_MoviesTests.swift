5c97f482ac4d538f62fe216eaa55e612885e4f69
Fabio Dantas <dantasirl@hotmail.com>
2018-09-27 16:05:39 +0100

    Initial Commit

---
+++ Best MoviesTests/Best_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Best_MoviesTests.swift
+//  Best MoviesTests
+//
+//  Created by Fabio Dantas on 27/09/2018.
+//  Copyright © 2018 Fabio Dantas. All rights reserved.
+//
+
+import XCTest
+@testable import Best_Movies
+
+class Best_MoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
